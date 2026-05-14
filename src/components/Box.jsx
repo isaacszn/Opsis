8788aa261ed7657f6d6b664d8bceccbfbf114596
Zik <isaaczik9@gmail.com>
2026-05-14 13:12:06 +0100

    update

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,7 +1,7 @@
 const Box = (props) => {
     return (
-        <div className="border-gray-800 p-5 w-[85%] lg:w-[330px] backdrop-blur-lg rounded-4xl shadow-md shadow-gray-800/50">
-            <div className="monospace text-black text-2xl font-bold border border-black-800 w-[40px] h-[40px] rounded-full flex items-center justify-center">
+        <div className="border-gray-800 p-5 w-[85%] lg:w-[330px] backdrop-blur-lg rounded-4xl shadow-md shadow-gray-800/50 hover:shadow-xl focus:shadow-xl active:shadow-xl transition-shadow cursor-pointer">
+            <div className="monospace text-blue-800 text-2xl font-bold border border-black-800 w-[40px] h-[40px] rounded-full flex items-center justify-center">
                 {props.number}
             </div>
             <div className="p-6">
