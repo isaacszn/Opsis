1d6b2c533547b2d6db08313fd4432d218f0a08af
Zik <isaaczik9@gmail.com>
2026-05-25 15:49:00 +0100

    update

--- src/pages/Response.jsx
+++ src/pages/Response.jsx
@@ -45,15 +45,16 @@ const Response = () => {
 
                 <div className="lg:w-[40%] lg:mx-auto lg:border lg:border-gray-300 lg:rounded-md my-10 lg:shadow-sm lg:shadow-black-500/80">
                     <div className="flex flex-col justify-center items-end me-5">
-                        <div className="bg-blue-800 w-[190px] h-[190px] lg:w-[230px] lg:h-[230px] rounded-md flex flex-col justify-center items-start my-15">
-                            <img src={image} alt="Photo" className="w-[160px] h-[140px] lg:w-[200px] lg:h-[180px] rounded-md m-2 text-center mx-auto" />
-                            <p className="text-sm poppins text-white ms-7"><span className="monospace font-bold">&gt;</span> What is this?</p>
+                        <div className="bg-blue-800 w-[220px] h-[220px] lg:w-[230px] lg:h-[230px] rounded-tr-sm rounded-tl-3xl rounded-bl-3xl rounded-br-3xl flex flex-col justify-center items-start my-15">
+                            <img src={image} alt="Photo" className="w-[200px] h-[200px] lg:w-[210px] lg:h-[210px] rounded-xl m-2 text-center mx-auto" />
                         </div>
                     </div>
 
                     <div className="flex flex-col justify-center items-start ms-5">
-                        <div className="border border-gray-300 p-5 w-[75%] lg:w-[60%] backdrop-blur-lg rounded-4xl shadow-sm shadow-gray-800/50 hover:shadow-md focus:shadow-md active:shadow-md transition-shadow cursor-pointer rounded-md mb-20">
-                            <p className="poppins text-gray-800 leading-relaxed text-center mx-auto">Opsis analyzing image...</p>
+                        <div className="border border-gray-300 p-5 w-[75%] lg:w-[60%] backdrop-blur-lg rounded-4xl shadow-sm shadow-gray-800/50 hover:shadow-md focus:shadow-md active:shadow-md transition-shadow cursor-pointer rounded-tl-sm rounded-tr-3xl rounded-bl-3xl rounded-br-3xl mb-20">
+                            <p className="serif text-gray-800 leading-relaxed text-center mx-auto">
+                                Opsis analyzing image...
+                            </p>
                         </div>
                     </div>
                 </div>
